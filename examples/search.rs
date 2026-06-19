e23e9061395957066d83bfea33e896b22fabc9ec
TaimWay <taimway@gmail.com>
2026-06-19 10:23:42 +0800

    style(examples): reformat imports to match rustfmt
    - Reorder use statements (quick_search, full_search, deep_search)
    - Wrap long print! string to stay within line width limits

--- examples/search.rs
+++ examples/search.rs
@@ -1,4 +1,4 @@
-use java_manager::{JavaInfo, quick_search, full_search, deep_search};
+use java_manager::{JavaInfo, deep_search, full_search, quick_search};
 use std::io::{self, Write};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -6,7 +6,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let mut input = String::new();
     loop {
-        print!("Do you want to search for Java installations using quick or deep search? (q: quick search/f: full search/d: deep search) ");
+        print!(
+            "Do you want to search for Java installations using quick or deep search? (q: quick search/f: full search/d: deep search) "
+        );
         io::stdout().flush().unwrap();
         input.clear();
         io::stdin().read_line(&mut input)?;
